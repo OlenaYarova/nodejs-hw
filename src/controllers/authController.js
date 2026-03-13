7e8f967a74ec2f363f7789becdb30a758ff1c2b4
Olena <marineelen91@gmail.com>
2026-03-13 23:31:54 +0200

    fix resetPassword controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -163,24 +163,27 @@ export const resetPassword = async (req, res) => {
   } catch {
     throw createHttpError(401, 'Invalid or expired token');
   }
-}
 
-const user = await User.findOne({
-  _id: payload.sub, email:
-    payload.email
-})
-if (!user) {
-  throw createHttpError(404, 'User not found');
-}
 
-const hashedPassword = await bcrypt.hash(password, 10);
-await User.updateOne(
-  { _id: user._id },
-  { password: hashedPassword });
+  const user = await User.findOne({
+    _id: payload.sub,
+    email: payload.email
+  })
 
-await Session.deleteMany({ userId: user._id });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
 
-res.status(200).json({
-  message: 'Password reset successfully'
-});
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.updateOne(
+    { _id: user._id },
+    { password: hashedPassword });
+
+  await Session.deleteMany({ userId: user._id });
+
+  res.status(200).json({
+    message: 'Password reset successfully'
+  });
+};
 
